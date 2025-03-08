9a4b18768ca1e45db14fc1a59da3da2923c91f3f
amraboelenin <amraboelenin70@gmail.com>
2025-03-08 13:23:54 +0200

    Applying the review coommand

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main{
 
  public static void main(String[] args){
    
-    System.out.println("Hello Hello"); 
-    System.out.println("Hello Hello2");
+    System.out.println("Hello Hello12"); 
+    
  
  }
 }
