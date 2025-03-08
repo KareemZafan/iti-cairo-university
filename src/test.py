63bc3c39f7ea330a49883056ee549f96ee0e49a5
KareemZafan <kareemm.zafan@gmail.com>
2025-03-08 14:48:17 +0200

    add sub traction

--- src/test.py
+++ src/test.py
@@ -4,9 +4,15 @@ import math
 def get_square_root(value):
 	return math.sqrt(value) 
 	
+def subtract(a , b):
+	return a - b
 
 
 
 def test_square_root():
 	assert 10 == get_square_root(100)
 	assert 25 == get_square_root(625)
+	
+def test_subtract():
+	assert 10 == subtract(100, 90)
+	
