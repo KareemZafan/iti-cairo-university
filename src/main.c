da2ceb9081f2e5c7c40e91f9cce41e56a05847e7
amraboelenin <amraboelenin70@gmail.com>
2025-03-08 14:30:52 +0200

    Edit

--- src/main.c
+++ src/main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+double multiply(double a, double b){
+    return a * b;
+}
+
+
+
+long abs (long a){
+    return a > 0 ? a : -a;
+}
 
 int main(){
+    int a = 10;
+    int b = 20;
 
-int a = 10 ; 
-int b = 20; 
+    printf("Hello World");
+    printf("%d + %d = %d", a, b, add(a, b));
 
-printf("Hello World"); 
-printf("%d + %d = %d" ,a,b,a+b);  // 10 + 20 = 30
+    // 10 + 20 = 30
 
-return 0; 
+    return 0;
 }
